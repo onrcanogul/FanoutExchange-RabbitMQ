6a51dd3fe884cddfdd716c1662e70db2130970e0
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2024-04-25 15:07:18 +0300

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://yejguzib:NuvHgMGxrkfxK8SHh3VRyAYLi3B3ENf1@shark.rmq.cloudamqp.com/yejguzib");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "fanout-exchange-example", type: ExchangeType.Fanout);
+
+
+Console.Write("Queue name: ");
+string queueName = Console.ReadLine();
+
+channel.QueueDeclare(
+    queue: queueName,
+    exclusive: false);
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "fanout-exchange-example",
+    routingKey: string.Empty
+    );
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+
+Console.Read();
+
+
+
+
---
+++ Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://yejguzib:NuvHgMGxrkfxK8SHh3VRyAYLi3B3ENf1@shark.rmq.cloudamqp.com/yejguzib");
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare("fanout-exchange-example", type: ExchangeType.Fanout);
+
+
+
+while (true)
+{
+    Console.Write("Message : ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "fanout-exchange-example", routingKey: string.Empty, body: byteMessage);
+}
+
+
+Console.Read();
+
+
+
+
